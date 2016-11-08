7f954dcea48da762071b040361ecc274f269ec4e
rainy <syuting_liu@163.com>
2016-11-08 17:58:29 +0800

    提交到git的代码2

--- app/src/main/java/com/example/rainy/gridviewdeemo/PhotoViewActivity.java
+++ app/src/main/java/com/example/rainy/gridviewdeemo/PhotoViewActivity.java
@@ -6,6 +6,9 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +22,9 @@ public class PhotoViewActivity extends AppCompatActivity {
 
     private Toast mCurrentToast;
 
+    static final String PHOTO_TAP_TOAST_STRING = "Photo Tap! X: %.2f %% Y:%.2f %% ID: %d";
+    static final String SCALE_TOAST_STRING = "Scaled to: %.2ff";
+    static final String FLING_LOG_STRING = "Fling velocityX: %.2f, velocityY: %.2f";
 
     private Matrix mCurrentDisplayMatrix = null;
     @Override
@@ -36,11 +42,59 @@ public class PhotoViewActivity extends AppCompatActivity {
         mAttacher = new PhotoViewAttacher(mImageView);
 
         // Lets attach some listeners, not required though!
-        mAttacher.setOnMatrixChangeListener(new PhotoViewAttacher.OnMatrixChangedListener() {
-            @Override
-            public void onMatrixChanged(RectF rect) {
+        mAttacher.setOnMatrixChangeListener(new MatrixChangeListener());
+        mAttacher.setOnPhotoTapListener(new PhotoTapListener());
+        mAttacher.setOnSingleFlingListener(new SingleFlingListener());
+    }
 
+    private class MatrixChangeListener implements PhotoViewAttacher.OnMatrixChangedListener{
+
+        @Override
+        public void onMatrixChanged(RectF rect) {
+            mCurrMatrixTv.setText(rect.toString());
+        }
+    }
+
+    private class PhotoTapListener implements PhotoViewAttacher.OnPhotoTapListener{
+
+        @Override
+        public void onPhotoTap(View view, float x, float y) {
+            float xPercentage = x * 100f;
+            float yPercentage = y * 100f;
+
+            showToast(String.format(PHOTO_TAP_TOAST_STRING, xPercentage, yPercentage, view == null ? 0 : view.getId()));
+        }
+
+        @Override
+        public void onOutsidePhotoTap() {
+            showToast("You have a tap event on the place where out of the photo.");
+        }
+    }
+
+    private void showToast(CharSequence text) {
+        if (null != mCurrentToast) {
+            mCurrentToast.cancel();
+        }
+
+        mCurrentToast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        mCurrentToast.show();
+    }
+
+    private class SingleFlingListener implements PhotoViewAttacher.OnSingleFlingListener{
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (BuildConfig.DEBUG) {
+                Log.d("PhotoView", String.format(FLING_LOG_STRING, velocityX, velocityY));
             }
-        });
+            return true;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Need to call clean-up
+        mAttacher.cleanup();
     }
 }
